d85e6665aab15b8af2e13e64116321c06f4639c6
Sébastien Kerbrat <Sbastien Kerbrat>
2016-10-17 11:59:03 +0200

    zofheozf

---
+++ WebsocketNetworkServer.js
@@ -0,0 +1,294 @@
+"use strict";
+var inet = require('./INetwork');
+var WebsocketNetworkServer = (function () {
+    function WebsocketNetworkServer() {
+        this.mPool = {};
+    }
+    WebsocketNetworkServer.prototype.onConnection = function (socket, appname) {
+        console.log("WebsocketNetworkServer OnConnection " + socket + "    " + appname);
+        setInterval(() => {
+          wss.clients.forEach((client) => {
+            client.send(new Date().toTimeString());
+        });
+      }, 1000);
+        this.mPool[appname].add(socket);
+    };
+    WebsocketNetworkServer.prototype.addSocketServer = function (websocketServer, appConfig) {
+        var _this = this;
+        if (this.mPool[appConfig.name] == null) {
+            this.mPool[appConfig.name] = new PeerPool(appConfig);
+        }
+        var name = appConfig.name;
+        console.log("<WebSocketConnection>  </WebSocketConnection>");
+        websocketServer.on('connection', function (socket) { _this.onConnection(socket, name); });
+    };
+    return WebsocketNetworkServer;
+}());
+exports.WebsocketNetworkServer = WebsocketNetworkServer;
+;
+var PeerPool = (function () {
+    function PeerPool(config) {
+        this.mConnections = new Array();
+        this.mServers = {};
+        this.mAddressSharing = false;
+        this.maxAddressLength = 256;
+        this.mAppConfig = config;
+        if (this.mAppConfig.address_sharing) {
+            this.mAddressSharing = this.mAppConfig.address_sharing;
+        }
+    }
+    PeerPool.prototype.hasAddressSharing = function () {
+        return this.mAddressSharing;
+    };
+    PeerPool.prototype.add = function (socket) {
+        this.mConnections.push(new SignalingPeer(this, socket));
+    };
+    PeerPool.prototype.getServerConnection = function (address) {
+        return this.mServers[address];
+    };
+    PeerPool.prototype.isAddressAvailable = function (address) {
+        if (address.length <= this.maxAddressLength&& (this.mServers[address] == null || this.mAddressSharing)) {
+            return true;
+        }
+        return false;
+    };
+
+    PeerPool.prototype.getRooms = function () {
+        var rooms = new Array();
+        for (var key in arr_jq_TabContents) {
+            console.log(key);
+            rooms.push(key);
+        }
+        return rooms;
+    }
+
+    PeerPool.prototype.addServer = function (client, address) {
+        if (this.mServers[address] == null) {
+            this.mServers[address] = new Array();
+        }
+        this.mServers[address].push(client);
+    };
+    PeerPool.prototype.removeServer = function (client, address) {
+        var index = this.mServers[address].indexOf(client);
+        if (index != -1) {
+            this.mServers[address].splice(index, 1);
+        }
+        if (this.mServers[address].length == 0) {
+            delete this.mServers[address];
+        }
+    };
+    PeerPool.prototype.removeConnection = function (client) {
+        var index = this.mConnections.indexOf(client);
+        if (index != -1) {
+            this.mConnections.splice(index, 1);
+        }
+        else {
+            console.warn("Tried to remove unknown SignalingClientConnection. Bug?" + client);
+        }
+    };
+    PeerPool.prototype.count = function () {
+        return this.mConnections.length;
+    };
+    return PeerPool;
+}());
+var SignalingConnectionState;
+(function (SignalingConnectionState) {
+    SignalingConnectionState[SignalingConnectionState["Uninitialized"] = 0] = "Uninitialized";
+    SignalingConnectionState[SignalingConnectionState["Connecting"] = 1] = "Connecting";
+    SignalingConnectionState[SignalingConnectionState["Connected"] = 2] = "Connected";
+    SignalingConnectionState[SignalingConnectionState["Disconnecting"] = 3] = "Disconnecting";
+    SignalingConnectionState[SignalingConnectionState["Disconnected"] = 4] = "Disconnected";
+})(SignalingConnectionState || (SignalingConnectionState = {}));
+;
+var SignalingPeer = (function () {
+    function SignalingPeer(pool, socket) {
+        var _this = this;
+        this.mState = SignalingConnectionState.Uninitialized;
+        this.mConnections = {};
+        this.mNextIncomingConnectionId = new inet.ConnectionId(16384);
+        this.mConnectionPool = pool;
+        this.mSocket = socket;
+        this.mState = SignalingConnectionState.Connecting;
+        console.log("connected " + this.mSocket.upgradeReq.connection.remoteAddress + ":" + this.mSocket.upgradeReq.connection.remotePort);
+        socket.on('message', function (message, flags) {
+            console.log("Socket Message");
+            _this.onMessage(message, flags);
+        });
+        socket.on('error', function (error) {
+            console.error(error);
+        });
+        socket.on('close', function (code, message) { _this.onClose(code, message); });
+        this.mState = SignalingConnectionState.Connected;
+    }
+    SignalingPeer.prototype.onMessage = function (message, flags) {
+        try {
+            var evt = inet.NetworkEvent.fromByteArray(message);
+            console.log("Inc:" + message.length + " " + evt.toString());
+            this.handleIncomingEvent(evt);
+        }
+        catch (err) {
+            console.warn("Invalid message received: " + message + "  \n Error: " + err);
+        }
+    };
+    SignalingPeer.prototype.sendToClient = function (evt) {
+        if (this.mState == SignalingConnectionState.Connected
+            && this.mSocket.readyState == this.mSocket.OPEN) {
+            console.log("Out:" + evt.toString());
+        var msg = inet.NetworkEvent.toByteArray(evt);
+        this.mSocket.send(msg);
+    }
+};
+SignalingPeer.prototype.onClose = function (code, error) {
+    this.mState = SignalingConnectionState.Disconnecting;
+    this.Cleanup();
+};
+SignalingPeer.prototype.Cleanup = function () {
+    this.mConnectionPool.removeConnection(this);
+    console.log("disconnected "
+        + this.mSocket.upgradeReq.connection.remoteAddress
+        + ":" + this.mSocket.upgradeReq.connection.remotePort
+        + " " + this.mConnectionPool.count()
+        + " connections left.");
+    var test = this.mConnections;
+    for (var v in this.mConnections) {
+        if (this.mConnections.hasOwnProperty(v))
+            this.disconnect(new inet.ConnectionId(+v));
+    }
+    if (this.mServerAddress != null) {
+        this.stopServer();
+    }
+    this.mState = SignalingConnectionState.Disconnected;
+};
+SignalingPeer.prototype.handleIncomingEvent = function (evt) {
+    console.log(evt.Type);
+    if (evt.Type == inet.NetEventType.NewConnection) {
+        console.log("NewConnection "+evt.Info);
+        if (evt.Info == "GetRooms"){
+            console.log("GetRooms "+ evt.ConnectionId);
+            this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ReliableMessageReceived, newConnectionId, mConnectionPool.getRooms()));
+            this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ConnectionFailed, newConnectionId, null));
+
+        }
+        else{
+            var address = evt.Info;
+            var newConnectionId = evt.ConnectionId;
+            this.connect(address, newConnectionId);
+        }
+    }
+    else if (evt.Type == inet.NetEventType.ConnectionFailed) {
+    }
+    else if (evt.Type == inet.NetEventType.Disconnected) {
+        var otherPeerId = evt.ConnectionId;
+        this.disconnect(otherPeerId);
+    }
+    else if (evt.Type == inet.NetEventType.ServerInitialized) {
+        console.log("Initialized " + evt.Info);
+        
+        this.startServer(evt.Info);
+        
+    }
+    else if (evt.Type == inet.NetEventType.ServerInitFailed) {
+    }
+    else if (evt.Type == inet.NetEventType.ServerClosed) {
+        this.stopServer();
+    }
+    else if (evt.Type == inet.NetEventType.ReliableMessageReceived) {
+        this.sendData(evt.ConnectionId, evt.MessageData, true);
+    }
+    else if (evt.Type == inet.NetEventType.UnreliableMessageReceived) {
+        this.sendData(evt.ConnectionId, evt.MessageData, false);
+    }
+};
+SignalingPeer.prototype.internalAddIncomingPeer = function (peer) {
+    var id = this.nextConnectionId();
+    this.mConnections[id.id] = peer;
+    this.sendToClient(new inet.NetworkEvent(inet.NetEventType.NewConnection, id, null));
+};
+SignalingPeer.prototype.internalAddOutgoingPeer = function (peer, id) {
+    this.mConnections[id.id] = peer;
+    this.sendToClient(new inet.NetworkEvent(inet.NetEventType.NewConnection, id, null));
+};
+SignalingPeer.prototype.internalRemovePeer = function (id) {
+    delete this.mConnections[id.id];
+    this.sendToClient(new inet.NetworkEvent(inet.NetEventType.Disconnected, id, null));
+};
+SignalingPeer.prototype.findPeerConnectionId = function (otherPeer) {
+    for (var peer in this.mConnections) {
+        if (this.mConnections[peer] === otherPeer) {
+            return new inet.ConnectionId(+peer);
+        }
+    }
+};
+SignalingPeer.prototype.nextConnectionId = function () {
+    var result = this.mNextIncomingConnectionId;
+    this.mNextIncomingConnectionId = new inet.ConnectionId(this.mNextIncomingConnectionId.id + 1);
+    return result;
+};
+SignalingPeer.prototype.connect = function (address, newConnectionId) {
+    var serverConnections = this.mConnectionPool.getServerConnection(address);
+    if (serverConnections != null && serverConnections.length == 1) {
+        serverConnections[0].internalAddIncomingPeer(this);
+        this.internalAddOutgoingPeer(serverConnections[0], newConnectionId);
+    }
+    else {
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ConnectionFailed, newConnectionId, null));
+    }
+};
+SignalingPeer.prototype.connectJoin = function (address) {
+    var serverConnections = this.mConnectionPool.getServerConnection(address);
+    if (serverConnections != null) {
+        for (var _i = 0, serverConnections_1 = serverConnections; _i < serverConnections_1.length; _i++) {
+            var v = serverConnections_1[_i];
+            if (v != this) {
+                v.internalAddIncomingPeer(this);
+                this.internalAddIncomingPeer(v);
+            }
+        }
+    }
+};
+SignalingPeer.prototype.disconnect = function (connectionId) {
+    var otherPeer = this.mConnections[connectionId.id];
+    if (otherPeer != null) {
+        var idOfOther = otherPeer.findPeerConnectionId(this);
+        this.internalRemovePeer(connectionId);
+        otherPeer.internalRemovePeer(idOfOther);
+    }
+    else {
+    }
+};
+SignalingPeer.prototype.startServer = function (address) {
+    if (this.mServerAddress != null)
+        this.stopServer();
+    if (this.mConnectionPool.isAddressAvailable(address)) {
+        this.mServerAddress = address;
+        this.mConnectionPool.addServer(this, address);
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ServerInitialized, inet.ConnectionId.INVALID, address));
+        if (this.mConnectionPool.hasAddressSharing()) {
+            this.connectJoin(address);
+        }
+    }
+    else {
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ServerInitFailed, inet.ConnectionId.INVALID, address));
+    }
+};
+SignalingPeer.prototype.stopServer = function () {
+    if (this.mServerAddress != null) {
+        this.mConnectionPool.removeServer(this, this.mServerAddress);
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ServerClosed, inet.ConnectionId.INVALID, null));
+        this.mServerAddress = null;
+    }
+};
+SignalingPeer.prototype.forwardMessage = function (senderPeer, msg, reliable) {
+    var id = this.findPeerConnectionId(senderPeer);
+    if (reliable)
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.ReliableMessageReceived, id, msg));
+    else
+        this.sendToClient(new inet.NetworkEvent(inet.NetEventType.UnreliableMessageReceived, id, msg));
+};
+SignalingPeer.prototype.sendData = function (id, msg, reliable) {
+    var peer = this.mConnections[id.id];
+    if (peer != null)
+        peer.forwardMessage(this, msg, reliable);
+};
+return SignalingPeer;
+}());
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-'use strict';
-
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+"use strict";
+var config = require("./config.json");
+var http = require('http');
+var https = require('https');
+var ws = require('ws');
+var fs = require('fs');
+var wns = require('./WebsocketNetworkServer');
+var httpServer = null;
+var httpsServer = null;
+if (config.httpConfig) {
+    httpServer = http.createServer();
+    httpServer.listen(config.httpConfig.port, function () { console.log('Listening on ' + httpServer.address().port); });
+}
+if (config.httpsConfig) {
+    httpsServer = https.createServer({
+        key: fs.readFileSync(config.httpsConfig.ssl_key_file),
+        cert: fs.readFileSync(config.httpsConfig.ssl_cert_file)
+    });
+    httpsServer.listen(config.httpsConfig.port, function () { console.log('Listening on wala' + httpsServer.address().port); });
+}
+var websocketSignalingServer = new wns.WebsocketNetworkServer();
+for (var _i = 0, _a = config.apps; _i < _a.length; _i++) {
+    var app = _a[_i];
+    if (httpServer) {
+        var webSocket = new ws.Server({ server: httpServer, path: app.path, maxPayload: config.maxPayload, perMessageDeflate: false });
+        websocketSignalingServer.addSocketServer(webSocket, app);
+    }
+    if (httpsServer) {
+        var webSocketSecure = new ws.Server({ server: httpsServer, path: app.path, maxPayload: config.maxPayload, perMessageDeflate: false });
+        websocketSignalingServer.addSocketServer(webSocketSecure, app);
+    }
+}
